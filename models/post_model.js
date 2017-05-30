64dd9ffcdfd4de853125a4abea574e0655d8073b
Mehdi Raash <leaksin@gmail.com>
2017-05-30 22:32:38 +0430

    post_model has been updated

--- models/post_model.js
+++ models/post_model.js
@@ -1,1 +1,16 @@
-d
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/demoCMS');
+var db = mongoose.connection;
+ 
+
+var postSchema = mongoose.Schema({
+    title: String,
+    text: String,  
+    createdAt: Date,
+    whoCreated: ObjectId
+});
+var User = mongoose.model('post', postSchema, 'post'); 
+
+module.exports = {
+
+};
